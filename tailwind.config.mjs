054a7d3917615c9e93c589013b23b34934d0abb5
leonelvalentinn <leonelvalentinn@gmail.com>
2024-02-27 18:45:38 -0600

    first commit

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,18 @@
+import animations from '@midudev/tailwind-animations'
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: '#0b3c3c',
+				secondary: '#0c6464',
+				third: '#148b8b',
+				dark: '#041919',
+				greenP: '#669999', 
+			},
+		},
 	},
-	plugins: [],
+	plugins: [animations],
 }
